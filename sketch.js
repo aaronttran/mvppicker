ac346981a9ad07a5d91a9c6cfa23714bcab10473
aaronttran <101088712+aaronttran@users.noreply.github.com>
2022-05-01 23:46:00 -0700

    Upload of assets and updated code

--- sketch.js
+++ sketch.js
@@ -1,7 +1,43 @@
+var click1;
+var img = [];
+let bg;
+
+function preload() {
+  for (let i = 0; i <= 7; i++){
+   img[i] = loadImage(`assets/player_${i}.JPG`)
+ }
+}
+
 function setup() {
-  createCanvas(400, 400);
+  bg = img[i];
+  createCanvas(1600, 800);
+  background(bg);
+  textSize(45);
+  textFont("Roboto Slab Black");
+  textAlign(CENTER);
+  textStyle(BOLD);
+  fill(255);
+
+  text("Who is Your NBA MVP This Season?", width / 2, height / 5.5);
+
+  click1 = new Clickable();
+  click1.locate(width / 2, height / 3);
+  click1.strokeWeight = 1;
+  click1.stroke = "#000000";
+  click1.text = "Start";
+  click1.textFont = "Segoe UI";
+  click1.textSize = 20;
+  click1.textScaled = true;
+  click1.cornerRadius = 18;
+  click1.onPress = function () {
+
+
+
+
+  }
 }
 
 function draw() {
-  background(220);
+  click1.draw();
+
 }
