f92e18dc6dde84d7432222fb4690d233422d598e
aaronttran <101088712+aaronttran@users.noreply.github.com>
2022-05-03 19:07:36 -0700

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,5 +1,9 @@
 let gameState = "title";
+//let canvas;
 var click1;
+var kDurant;
+var jTatum;
+var dBooker;
 let timerStart = 0; //variable to hold the start time of each iteration of the timer
 let timerLength = 100; //length of the timer (in milliseconds)
 let timerCount = 0; //number of times the timer has reset
@@ -17,17 +21,20 @@ function preload(){
 
 function setup() {
   createCanvas(displayWidth, displayHeight);
-  textSize(100);
+//  canvas.mousePressed(mousePressed);
+//  canvas.parent("sketch-holder");
+//  frameRate(60);
+
+  textSize(70);
   textFont("Segoe UI");
   textAlign(CENTER);
   textStyle(BOLD);
   fill(255);
 
-  push();
   click1 = new Clickable();
-  click1.locate(0, 0);
-  click1.width = displayWidth;
-  click1.height = displayHeight;
+  click1.locate(width/2, height/2);
+  click1.width = width/8;
+  click1.height = height/8;
   click1.color = "#FFFFFF00";
   click1.text = "Click Anywhere to Start";
   click1.textColor = "#FFFFFF";
@@ -35,65 +42,69 @@ function setup() {
   click1.textSize = 30;
   click1.textScaled = true;
   click1.cornerRadius = 18;
-  pop();
-//  click1.onPress = function() {
+  click1.onPress = function() {
+    if (gameState === "title") {
+      gameState = "starter";
+    }
+  }
 
-//  }
-/*
-  imageButton = new Clickable();
-  imageButton.x = 0;
-  imageButton.y = 0;
-  imageButton.width = 1600;
-  imageButton.height = 800;
-  imageButton.image = img;
-  imageButton.fitImage = true;
-*/
+
+  kDurant = new Clickable();
+  kDurant.x = width / 4;
+  kDurant.y = height/2;
+  kDurant.width = width / 2;
+  kDurant.height = height / 2;
+
+  jTatum = new Clickable();
+  jTatum.x = width / 8;
+  jTatum.y = height/2;
+  jTatum.width = width / 2;
+  jTatum.height = height / 2;
+
+  dBooker = new Clickable();
+  dBooker.x = width / 1.3;
+  dBooker.y = height/2;
+  dBooker.width = width / 2;
+  dBooker.height = height / 2;
 }
 
 function draw() {
-  titleScreen();
-/*  switch (gameState) {
+  switch (gameState) {
     case "title":
       titleScreen();
       break;
     case "starter":
-      gameStage1();
+      background(200);
+//      gameStage1();
       break;
-    case "choose1":
+/*    case "durant":
+      gameChoice1();
+      break;
+    case "tatum":
+      gameChoice2();
+      break;
+    case "booker":
+      gameChoice3();
+    case "picker1":
       gameStage2();
       break;
-    case "choose2":
-      gameStage3();
-      break;
-    case "choose3":
-      gameStage4();
     case "winner":
       gameOver();
       break;
+      */
+  }
   }
 
-  click1.draw();
-  text("Who is Your NBA MVP This Season?", width / 2, height / 2.3);
-  if (millis() - timerStart > timerLength){
-    timerCount++; //increment the number of times the timer has reset
+/*function mousePressed() {
 
-    //reset timerCount so you don't overrun the number of items in your array
-    if (timerCount > numImgs){
-      timerCount = 1;
-    }
-
-    //draw images to the canvas (the minus one is because arrays start their indexing from 0 not from 1)
-    imageMode(CENTER);
-    image(img[timerCount - 1], width/2, height/2, displayWidth, displayHeight);
-    filter(BLUR, 4);
-
-    timerStart = millis(); //reset start time of timer to new current time
-  */
-  }
+  gameState = "starter";
+}
+*/
 
   function titleScreen() {
     click1.draw();
     text("Who is Your NBA MVP This Season?", width / 2, height / 2.3);
+    push();
     if (millis() - timerStart > timerLength){
       timerCount++; //increment the number of times the timer has reset
 
@@ -105,12 +116,19 @@ function draw() {
       //draw images to the canvas (the minus one is because arrays start their indexing from 0 not from 1)
       imageMode(CENTER);
       image(img[timerCount - 1], width/2, height/2, displayWidth, displayHeight);
-      filter(BLUR, 4);
+      filter(BLUR, 5);
 
       timerStart = millis(); //reset start time of timer to new current time
   }
+  pop();
+
 /*
   function gameStage1() {
+    kDurant.draw();
+    jTatum.draw();
+    dBooker.draw();
+    background("#FFFFFF");
+    text("Select One Player to Advance");
 
   }
 
